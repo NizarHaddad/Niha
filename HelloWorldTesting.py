a39cc5570aea4e3360116f6ccdff6aeb03d26834
NizarHaddad <nizooh@gmail.com>
2017-11-07 17:38:51 +0200

    Update HelloWorldTesting.py

--- HelloWorldTesting.py
+++ HelloWorldTesting.py
@@ -1,6 +1,26 @@
 from HelloWorld import HelloWorld
 
 
-def test_hello_world():
+def test1_342340():
    cn=HelloWorld()
    assert(cn.print_messge() == "hello world !")
+   
+def test2_342341():
+   cn=HelloWorld()
+   assert(cn.print_messge() == "hellos world !")
+   
+def test3_342342():
+   cn=HelloWorld()
+   assert(cn.print_messge() == "hello world !")
+   
+def test4_342343():
+   cn=HelloWorld()
+   assert(cn.print_messge() == "hello world !")
+   
+def test5_342344():
+   cn=HelloWorld()
+   assert(cn.print_messge() == "hellod world !")
+   
+def test6_34235():
+   cn=HelloWorld()
+   assert(cn.print_messge() == "hellod world !")
